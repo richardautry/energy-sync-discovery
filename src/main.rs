b9b917069973e90391c3d3beaee9ea7d125ee264
Richard Autry <autry.richard@gmail.com>
2023-09-12 07:01:13 -0400

    Attempting to register the service correctly

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,67 @@
-use mdns_sd::{ServiceDaemon, ServiceEvent};
+use mdns_sd::{ServiceDaemon, ServiceEvent, ServiceInfo};
 use futures_util::{pin_mut, stream::StreamExt};
 use mdns::{Error, Record, RecordKind};
 use std::{net::IpAddr, time::Duration};
 
 fn main() {
     // Create a daemon
+    // let mdns = ServiceDaemon::new().expect("Failed to create daemon");
+
+    // // Browse for a service type.
+    // // TODO: How to get this to browse wifi network and not local network?
+    // let service_type = "_http._udp.local.";
+    // let receiver = mdns.browse(service_type).expect("Couldn't browse");
+
+    // // Receive the browse events in sync or async. Here is
+    // // an example of using a thread. Users can call `receiver.recv_async().await`
+    // // if running in async environment.
+    // // std::thread::spawn(move || {
+    // println!("Starting loop");
+    // while let Ok(event) = receiver.recv() {
+    //     println!("OK");
+    //     match event {
+    //         ServiceEvent::ServiceResolved(info) => {
+    //             println!("Resolved a new service: {} {} {}", info.get_fullname(), info.get_hostname(), info.get_port());
+
+    //             println!("{:?}", info.get_addresses());
+    //         }
+    //         other_event => {
+    //             println!("Received other event: {:?}", &other_event);
+    //         }
+    //     }
+    // };
+    register_service();
+}
+
+fn register_service() {
+
+    // Create a daemon
     let mdns = ServiceDaemon::new().expect("Failed to create daemon");
 
-    // Browse for a service type.
-    // TODO: How to get this to browse wifi network and not local network?
-    let service_type = "_energy-sync._udp.local.";
-    let receiver = mdns.browse(service_type).expect("Couldn't browse");
+    // Create a service info.
+    let service_type = "_http._udp.local.";
+    let instance_name = "my-instance";
+    // let host_ipv4 = "192.168.1.12";
+    let host_ipv4 = "0.0.0.0";
+    // let host_name = "192.168.1.12.local.";
+    let host_name = "my-instance.local.";
+    // TODO: This basically works, but how to discover service on wifi?
+    // It doesn't seem like this is broadcasting on wifi but instead looking at localhost (this computer only)
+    let port = 3000;
+    let properties = [("property_1", "test"), ("property_2", "1234")];
 
-    // Receive the browse events in sync or async. Here is
-    // an example of using a thread. Users can call `receiver.recv_async().await`
-    // if running in async environment.
-    // std::thread::spawn(move || {
-    println!("Starting loop");
-    while let Ok(event) = receiver.recv() {
-        println!("OK");
-        match event {
-            ServiceEvent::ServiceResolved(info) => {
-                println!("Resolved a new service: {} {} {}", info.get_fullname(), info.get_hostname(), info.get_port());
+    let my_service = ServiceInfo::new(
+        service_type,
+        instance_name,
+        host_name,
+        host_ipv4,
+        port,
+        &properties[..],
+    ).unwrap();
 
-                println!("{:?}", info.get_addresses());
-            }
-            other_event => {
-                println!("Received other event: {:?}", &other_event);
-            }
-        }
-    };
+    // Register with the daemon, which publishes the service.
+    mdns.register(my_service).expect("Failed to register our service");
+    println!("Finished registering");
 }
 
 /// The hostname of the devices we are searching for.
